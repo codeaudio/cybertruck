332e9ca33fa5f1ff560fbaf7a24c14bdac5a46f5
codeaudio <64453492+codeaudio@users.noreply.github.com>
2020-08-09 23:01:06 +0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,5 @@
+$("body").on('click', '[href*="#"]', function(e){
+  const fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 800);
+  e.preventDefault();
+});
